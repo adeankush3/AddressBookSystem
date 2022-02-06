72f62cc4cb1d0994eddc7f4bd599d9d7e4b3236f
adeankush3 <adeankush3@gmail.com>
2022-02-06 16:16:16 +0530

    [Ankush]Create A contact

--- AddressBookSystem/AddressBookSystem/Contacts.cs
+++ AddressBookSystem/AddressBookSystem/Contacts.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    internal class Contacts
+    public class Contacts
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
     }
 }
